e2aa8a8434617f9ee6576ea15a8f4acbc93b2ca0
bradysm <bradysm@vt.edu>
2019-09-05 17:09:21 -0400

    eay problem, that was rated as a hard. Need to look into if there is something faster, but pretty confident in this

---
+++ wordDistance.py
@@ -0,0 +1,28 @@
+# Good morning! Here's your coding interview problem for today.
+
+# Find an efficient algorithm to find the smallest distance (measured in number of words) between any two given words in a string.
+
+# For example, given words "hello", and "world" and a 
+# text content of "dog cat hello cat dog dog hello cat world", 
+# return 1 because there's only one word "cat" in between the two words.
+
+
+import sys
+def word_distance(string, word1, word2):
+    # split the string at the space
+    w1_i = w2_i = -1 # start them at -1 for not being seen
+    distance = sys.maxsize
+    for i, w in enumerate(string.split(' ')):
+        # update w1_i or w2_i if is the same 
+        w1_i = i if w == word1 else w1_i # update the w1_i
+        w2_i = i if w == word2 else w2_i # update the w2_i
+        both_words_seen = True if w1_i is not -1 and w2_i is not -1 else False
+        
+        if both_words_seen:
+            distance = min(distance, abs(w1_i - w2_i)-1)
+
+    return distance
+
+
+
+print(word_distance("dog cat hello cat dog dog hello cat world", "hello", "world"))
