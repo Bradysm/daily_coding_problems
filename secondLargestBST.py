3f167f0d0dc63499a7e1c609fc8a107c6678d241
bradysm <bradysm@vt.edu>
2020-08-03 13:16:37 -0400

    added my thought process to the code

--- secondLargestBST.py
+++ secondLargestBST.py
@@ -14,6 +14,29 @@ largest value
           13
 
 so here we would return 13
+
+
+The thought process for this is that the largest node in a BST is the node all the way to the right
+so if we keep taking the option to go to the right, and when we get to the node that does not 
+allow us to move any farther right, that is the maximum value in the BST. So, the second largest node
+in the tree, must be really close to that node, right? Well, kind of! If the maximum node is itself
+a leaf node, then the node previous to that is the second largest node in the tree. But what happens
+if that node is not a leaf node? Well, let's think about it, it could not have a subtree to the right
+because that would mean that there is a node with a value greater than the current node which we're considering
+the maximum, so that would contradict our claim of the node being the max node. So, the only case could be,
+is if there is a left subtree. Now, if we think about the node that led us to the maximum node, (10 in the diagram above)
+everything to the right of this node is greater than it, so returning 10 in this case would be wrong, because
+we know that 13 is the second largest node. So, we can see, that if the max node has a left subtree, then we
+want to return the maximum node from the left subtree, as that will be the second largest node, and NOT 10.
+If the max node does not have a left subtree, then we will return the node that is previous to the max node.
+
+
+So, this is simple. move as far to the right as possible, keep track of the current a prev node,
+once there is no more moves to the right, check to see if there is a left subtree on the current node
+if there isn't, then return prev, otherwise, return the max node from the left subtree. We then know this
+algorithm is bounded by the height of the tree so the algorithm runs in O(h) time where in the best
+case will be O(logn) (balanced tree) and in the worst case will be O(n) (essentially a linked list) and O(1) space.
+
 """
 
 # tree node
